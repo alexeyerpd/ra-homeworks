9f77987d9d7a2ef5afbc106a5782ffaf766b7739
alexeyerpd <alexeyerpd@gmail.com>
2018-06-22 23:12:22 +0300

    update

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,39 @@
 'use strict';
+
+function AuthForm({onAuth}) {
+  return (
+    <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={onAuth}>
+      <div className="Input">
+        <input required type="text" placeholder="Имя" />
+        <label></label>
+      </div>
+      <div className="Input">
+        <input type="email" placeholder="Электронная почта" onChange={checkMail}/>
+        <label></label>
+      </div>
+      <div className="Input">
+        <input required type="password" placeholder="Пароль" onChange={checkNum}/>
+        <label></label>
+      </div>
+      <button type="submit">
+        <span>Войти</span>
+        <i className="fa fa-fw fa-chevron-right"></i>
+      </button>
+    </form>
+  );
+}
+
+function checkMail(event) {
+  event.preventDefault();
+  event.isDefaultPrevented();
+  const target = event.currentTarget;
+  target.value = target.value.replace(/[^A-Za-z0-9@_.\-]/gi, '');
+}
+
+function checkNum(event) {
+  event.preventDefault();
+  event.isDefaultPrevented();
+  const target = event.currentTarget;
+  target.value = target.value.replace(/\W/gi, '');
+}
+
--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -1,1 +1,54 @@
 'use strict';
+
+function FeedbackForm ({data, onSubmit}) {
+  const forSubmit = (e) => {
+    e.preventDefault();
+    return JSON.stringify(data);
+  };
+
+  return (
+    <form className="content__form contact-form" onSubmit={forSubmit}>
+      <div className="testing">
+        <p>Чем мы можем помочь?</p>
+      </div>
+      <div className="contact-form__input-group" onChange={(e) => data.salutation = e.target.value}>
+        <input className="contact-form__input contact-form__input--radio" id="salutation-mr" name="salutation" type="radio" value="Мистер" defaultChecked={data.salutation === 'Мистер'}/>
+        <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
+        <input className="contact-form__input contact-form__input--radio" id="salutation-mrs" name="salutation" type="radio" value="Мисис" defaultChecked={data.salutation === 'Мисис'}/>
+        <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">Мисис</label>
+        <input className="contact-form__input contact-form__input--radio" id="salutation-ms" name="salutation" type="radio" value="Мис" defaultChecked={data.salutation === 'Мис'}/>
+        <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">Мис</label>
+      </div>
+      <div className="contact-form__input-group">
+        <label className="contact-form__label" htmlFor="name">Имя</label>
+        <input className="contact-form__input contact-form__input--text" id="name" name="name" type="text" value={data.name} onChange={(e) => data.name = e.target.value}/>
+      </div>
+      <div className="contact-form__input-group">
+        <label className="contact-form__label" htmlFor="email">Адрес электронной почты</label>
+        <input className="contact-form__input contact-form__input--email" id="email" name="email" type="email" value={data.email} onChange={(e) => data.email = e.target.value}/>
+      </div>
+      <div className="contact-form__input-group">
+        <label className="contact-form__label" htmlFor="subject">Чем мы можем помочь?</label>
+        <select className="contact-form__input contact-form__input--select" id="subject" name="subject" defaultValue={data.subject} onChange={(e) => data.subject = e.target.value}>
+          <option>У меня проблема</option>
+          <option>У меня важный вопрос</option>
+        </select>
+      </div>
+      <div className="contact-form__input-group">
+        <label className="contact-form__label" htmlFor="message">Ваше сообщение</label>
+        <textarea className="contact-form__input contact-form__input--textarea" id="message" name="message" rows="6" cols="65" value={data.message} onChange={(e) => data.message = e.target.value}></textarea>
+      </div>
+      <div className="contact-form__input-group" onChange={(e) => data.snacks = e.target.value}>
+        <p className="contact-form__label--checkbox-group">Хочу получить:</p>
+        <input className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца" defaultChecked={data.snacks.includes('пицца')}/>
+        <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
+        <input className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог" defaultChecked={data.snacks.includes('пирог')}/>
+        <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
+      </div>
+      <button className="contact-form__button" type="submit">Отправить сообщение!</button>
+      <output id="result" />
+    </form>
+  )
+}
+
+

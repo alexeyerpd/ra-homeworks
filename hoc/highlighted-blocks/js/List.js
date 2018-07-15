3eff6fc865b390249ac203784a52baf4c4580d26
alexeyerpd <alexeyerpd@gmail.com>
2018-07-16 00:32:43 +0300

    update

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -1,17 +1,36 @@
 'use strict';
 
 const List = props => {
-    return props.list.map(item => {
-        switch (item.type) {
-            case 'video':
-                return (
-                    <Video {...item} />
-                );
+  return props.list.map(item => {
+    switch (item.type) {
+      case 'video':
+        checker(item);
+        return (
+          checker(item, Video)
+        );
 
-            case 'article':
-                return (
-                    <Article {...item} />
-                );
-        }
-    });
+        case 'article':
+          return (
+            checker(item, Article)
+          );
+    }
+  });
 };
+
+function checker(props, Component) {
+  if (props.views >= 1000) {
+    return (
+      <Popular>
+        <Component {...props} />
+      </Popular>
+    )
+  } else if (props.views <= 100) {
+    return (
+      <New>
+        <Component {...props} />
+      </New>
+    )
+  } else {
+    return <Component {...props} />
+  }
+}

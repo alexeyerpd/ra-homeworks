55063185d38d5ae84910e936e5ce0a09e1e1ad1b
alexeyerpd <alexeyerpd@gmail.com>
2018-08-06 21:59:18 +0300

    update

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -1,36 +1,42 @@
 'use strict';
 
+const сhecker = props => Component => {
+  return class extends React.Component {
+    validator(props) {
+      if (props.views >= 1000) {
+        return (
+          <Popular>
+            <Component {...props} />
+          </Popular>
+        );
+      } else if (props.views <= 100) {
+        return (
+          <New>
+            <Component {...props} />
+          </New>
+        );
+      } else {
+        return <Component {...props} />;
+      }
+    }
+
+    render() {
+      return this.validator(props);
+    }
+  };
+};
+
 const List = props => {
   return props.list.map(item => {
+    let ListItem;
     switch (item.type) {
-      case 'video':
-        checker(item);
-        return (
-          checker(item, Video)
-        );
+      case "video":
+        ListItem = сhecker(item)(Video);
+        return <ListItem />;
 
-        case 'article':
-          return (
-            checker(item, Article)
-          );
+      case "article":
+        ListItem = сhecker(item)(Article);
+        return <ListItem />;
     }
   });
 };
-
-function checker(props, Component) {
-  if (props.views >= 1000) {
-    return (
-      <Popular>
-        <Component {...props} />
-      </Popular>
-    )
-  } else if (props.views <= 100) {
-    return (
-      <New>
-        <Component {...props} />
-      </New>
-    )
-  } else {
-    return <Component {...props} />
-  }
-}
--- hoc/relative-time/js/DateTime.js
+++ hoc/relative-time/js/DateTime.js
@@ -1,34 +1,30 @@
 'use strict';
 
 const DateTime = props => {
-  return (
-    <p className="date">{props.date}</p>
-  )
+  return <p className="date">{props.date}</p>;
 };
 
-function newFormat(Valid, Component) {
+function newFormat(Component) {
   return class extends React.Component {
+    changeDateTime(dataString) {
+      const currentDate = new Date();
+      const comparableDate = new Date(dataString);
+      const hourseFactor = 1000 * 60 * 60;
+      const differeneceTime = (currentDate - comparableDate) / hourseFactor;
 
-    render () {
-      const date = Valid(this.props.date);
-      return <Component date={ date } />
+      if (differeneceTime > 0 && differeneceTime < 1) {
+        return `${Math.ceil(differeneceTime * 60)} минут назад`;
+      } else if (differeneceTime > 1 && differeneceTime < 24) {
+        return `${Math.ceil(differeneceTime)} часов назад`;
+      } else if (differeneceTime > 24) {
+        return `${Math.ceil(differeneceTime / 24)} дней назад`;
+      }
     }
-  }
+
+    render() {
+      return <Component date={this.changeDateTime(this.props.date)} />;
+    }
+  };
 }
 
-function ChangeDateTime(dataString) {
-  const currentDate = new Date();
-  const comparableDate = new Date(dataString);
-  const hourseFactor = 1000 * 60 * 60;
-  const differeneceTime = (currentDate - comparableDate) / hourseFactor;
-
-  if (differeneceTime > 0 && differeneceTime < 1) {
-    return `${Math.ceil(differeneceTime * 60)} минут назад`;
-  } else if (differeneceTime > 1 && differeneceTime < 24) {
-    return `${Math.ceil(differeneceTime)} часов назад`;
-  } else if (differeneceTime > 24) {
-    return `${Math.ceil(differeneceTime / 24)} дней назад`;
-  }
-}
-
-const DateTimePretty = newFormat(ChangeDateTime, DateTime);
+const DateTimePretty = newFormat(DateTime);
